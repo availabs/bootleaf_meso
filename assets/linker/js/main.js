5a2f139f41777a09e4066ee91a2af35c70cba776
Matt Wolkoff <matt.wolkoff@gmail.com>
2014-04-29 14:34:27 -0400

    maybe done? starting work on typeahead/search

--- assets/linker/js/main.js
+++ assets/linker/js/main.js
@@ -6,17 +6,15 @@ var mesonet = {
     var mapquestOSM = L.tileLayer("http://{s}.tiles.mapbox.com/v3/am3081.h0po4e8k/{z}/{x}/{y}.png");
      
     var mapquestOAM = L.tileLayer("http://{s}.tiles.mapbox.com/v3/am3081.h0pml9h7/{z}/{x}/{y}.png", {
-      maxZoom: 20,
-      //subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"],
+      maxZoom: 19,
     });
     var mapquestHYB = L.layerGroup([L.tileLayer("http://{s}.tiles.mapbox.com/v3/am3081.h0pml9h7/{z}/{x}/{y}.png", {
-      maxZoom: 20,
-      //subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"]
+      maxZoom: 19,
     }), L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/hyb/{z}/{x}/{y}.png", {
       maxZoom: 19,
       subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"],
     })]);
-
+    /*Leaflet Tile Overlay Layers*/
     var rainfall = L.tileLayer('http://vis.availabs.org/mesonet/data/tiles/{z}/{x}/{y}.png', {minZoom: 5, maxZoom: 10,tms: true,opacity:0.5});
     var floodplains = L.tileLayer('http://vis.availabs.org/mesonet/data/flood_planes/{z}/{x}/{y}.png', {minZoom: 5, maxZoom: 12,tms: true,opacity:1});
     
@@ -26,37 +24,25 @@ var mesonet = {
       layers: [mapquestOSM]
     });
 
-   
-
     var baseLayers = {
       "Street Map": mapquestOSM,
       "Aerial Imagery": mapquestOAM,
       "Imagery with Streets": mapquestHYB,
-      //"Rainfall": rainfall,
-      //"Flood Plains": floodplains
     };
-    
    
-    /*var overlayMaps = {
-    "<span class='rainfall'>Rainfall</span>":rainfall,
-    "<span class='floodplains'>Flood Hazard Zones</span>":floodplains
-
-    };
-    */
-    
     var layerControl = L.control.layers(baseLayers, {}, {
       collapsed: true
     }).addTo(mesonet.map);
 
-    
     L.control.scale().addTo(mesonet.map);
-   
+    /*load and differentiate leaflet-tile layers for use with sidebar layer control*/
     mesonet.map.addLayer(rainfall);
     $('.leaflet-tile-pane .leaflet-layer').last().css('z-index',4).attr('id','rainfall').hide();
     mesonet.map.addLayer(floodplains);
     $('.leaflet-tile-pane .leaflet-layer').last().css('z-index',4).attr('id','floodplain').hide();   
 
-     var sidebarLoaded = false; 
+    /*Necessary for Loading Sidebar Properly*/
+    var sidebarLoaded = false; 
     $("#layers").click(function(){
       if(!sidebarLoaded){
         mesonet.sidebar = L.control.sidebar('sidebar', { position: 'left'});
@@ -67,18 +53,11 @@ var mesonet = {
         mesonet.sidebar.toggle();
       }
     });
-
-
     
-    $( "#zoom" ).click(function(map) {
+    /* Functionality for Zoom to Extent*/
+  $( "#zoom" ).click(function(map) {
       mesonet.map.setView([42.76314586689494,-74.7509765625], 7);
-    });
-      /* Highlight search box text on click */
-    /*
-    $("#searchbox").click(function () {
-      $(this).select();
-    });*/
-   
+  });
 
     popup.init();
     /* Placeholder hack for IE */
